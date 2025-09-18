62d170ca6b91ff3041a9446b230ed1ca7baf4446
rajeevansharan <rajeevansharan@gmail.com>
2025-09-18 20:11:08 +0530

    Add ChatController to handle chat messages

---
+++ src/main/java/com/Project/ChatApplication/ChatController.java
@@ -0,0 +1,15 @@
+package com.Project.ChatApplication;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/message")
+    public String sendMessage(String message) {
+        return "Server received: " + message;
+    }
+}
